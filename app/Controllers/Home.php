5bb8c0a5a222f6d434ee269944cb2b1dee07c271
IqbalMaulanaHakim05 <iqbalmaulanahakim05@gmail.com>
2025-07-03 17:33:31 +0700

    menambahkan api data transaksi pembelian

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,17 +2,23 @@
 
 namespace App\Controllers;
 
-use App\Models\ProductModel; 
+use App\Models\ProductModel;
+use App\Models\TransactionModel;
+use App\Models\TransactionDetailModel; 
 
 class Home extends BaseController
 {
     protected $Product;
+    protected $Transaction;
+    protected $Transaction_Detail;
 
     function __construct()
     {
         helper('form');
         helper('number');
         $this->product = new ProductModel();
+        $this->Transaction = new TransactionModel();
+        $this->Transaction_Detail = new TransactionDetailModel();
     }
 
     public function index(): string
@@ -27,4 +33,29 @@ class Home extends BaseController
     {
         return view('v_faq');
     }
+
+    public function profile()
+{
+    $username = session()->get('username');
+    $data['username'] = $username;
+
+    $buy = $this->transaction->where('username', $username)->findAll();
+    $data['buy'] = $buy;
+
+    $product = [];
+
+    if (!empty($buy)) {
+        foreach ($buy as $item) {
+            $detail = $this->transaction_detail->select('transaction_detail.*, product.nama, product.harga, product.foto')->join('product', 'transaction_detail.product_id=product.id')->where('transaction_id', $item['id'])->findAll();
+
+            if (!empty($detail)) {
+                $product[$item['id']] = $detail;
+            }
+        }
+    }
+
+    $data['product'] = $product;
+
+    return view('v_profile', $data);
+}
 }
